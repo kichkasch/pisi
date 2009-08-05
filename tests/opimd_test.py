b2eb19fa45e4a966b09aaba4b0b9fa28bc6d57b3
kichkasch <kichkasch@gmx.de>
2009-08-05 18:17:06 +0000

    sucessful connection to opimd backend (read and write) - although, the fields are not clear yet (inforamtion about that is missing from the developer).
    git-svn-id: https://svn.projects.openmoko.org/svnroot/pisi/trunk@128 1ef7e37b-0359-4df3-b3cf-ae16f6d7f74b

--- tests/opimd_test.py
+++ tests/opimd_test.py
@@ -1,4 +1,39 @@
+"""
+background information - database schema
+
+root@om-gta02 ~/programming $ sqlite3  /etc/freesmartphone/opim/sqlite-contacts.db 
+SQLite version 3.6.5
+Enter ".help" for instructions
+Enter SQL statements terminated with a ";"
+sqlite> .schema
+CREATE TABLE contact_values (id INTEGER PRIMARY KEY, contactId INTEGER, Field TEXT, Value TEXT);
+CREATE TABLE contacts (
+                id INTEGER PRIMARY KEY,
+                Name TEXT,
+                Surname TEXT,
+                Nickname TEXT,
+                Birthdate TEXT,
+                MarrDate TEXT,
+                Partner TEXT,
+                Spouse TEXT,
+                MetAt TEXT,
+                HomeLoc TEXT,
+                Department TEXT,
+                refid TEXT,
+                deleted INTEGER DEFAULT 0);
+CREATE INDEX contact_values_contactId_idx ON contact_values (contactId);
+CREATE INDEX contacts_Name_idx ON contacts (Name);
+CREATE INDEX contacts_Nickname_idx ON contacts (Nickname);
+CREATE INDEX contacts_Surname_idx ON contacts (Surname);
+CREATE INDEX contacts_id_idx ON contacts (id);
+sqlite> root@om-gta02 ~/programming $ 
+"""
+
+
 import dbus, e_dbus
+import warnings
+warnings.filterwarnings('ignore', '.*')
+
 
 def testLoad():
     bus = dbus.SystemBus(mainloop = e_dbus.DBusEcoreMainLoop()) 
@@ -10,14 +45,31 @@ def testLoad():
 
     dbusObject = bus.get_object("org.freesmartphone.opimd", query)
     query = dbus.Interface(dbusObject, dbus_interface="org.freesmartphone.PIM.ContactQuery")
-
     count = query.GetResultCount()
-
     print count,  " entries in total."
+    
     for contact in query.GetMultipleResults(count):
-        print contact.get('Name'),  contact.get('Phone')
+        dbusObject = bus.get_object("org.freesmartphone.opimd", contact.get('Path'))
+        contactObject = dbus.Interface(dbusObject, dbus_interface="org.freesmartphone.PIM.Contact")
+        
+        if contactObject.GetUsedBackends()[0]!= "SQLite-Contacts":
+            continue    # let's only go for sqlite entries
+        print contactObject.GetContent()
+#        print contact.get('Name'),  contact.get('Phone')
+
+
         
 def testWrite():
-    pass
-    
+    bus = dbus.SystemBus(mainloop = e_dbus.DBusEcoreMainLoop()) 
+    dbusObject = bus.get_object("org.freesmartphone.opimd", "/org/freesmartphone/PIM/Contacts")
+    contacts = dbus.Interface(dbusObject, dbus_interface="org.freesmartphone.PIM.Contacts")
+
+    contact = {}
+    contact['Phone'] = "12345"
+    contact['Name'] = "Michael"
+    contact['Surname'] = 'Pilgermann'
+    contact['Email'] = 'michael.pilgermann@gmx.de'
+    path = contacts.Add(contact)
+
+#testWrite()
 testLoad()
