85576f1c606ae2bd186e872b2165ba1a67ef373c
kichkasch <kichkasch@gmx.de>
2009-12-19 21:52:54 +0000

    Evolution support well on the way; towards Freerunner working already ...
    git-svn-id: https://svn.projects.openmoko.org/svnroot/pisi/trunk@160 1ef7e37b-0359-4df3-b3cf-ae16f6d7f74b

--- modules/contacts_evolution.py
+++ modules/contacts_evolution.py
@@ -3,9 +3,6 @@ Sync contact information with Evolution on a Desktop.
 
 This file is part of Pisi.
 
-Implementation is based on *evolution-python* (L{http://live.gnome.org/Conduit/evolution-python}, 
-L{http://www.johnstowers.co.nz/blog/index.php/2007/05/30/python-bindings-for-evolution/}).
-
 Pisi is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
@@ -23,6 +20,10 @@ along with Pisi.  If not, see <http://www.gnu.org/licenses/>.
 from contacts import contacts
 from pisiconstants import *
 import pisiprogress
+
+import bsddb
+import vobject  
+from vobjecttools import *
 
 class SynchronizationModule(contacts.AbstractContactSynchronizationModule):
     def __init__( self, modulesString, config, configsection, folder, verbose=False, soft=False):
@@ -42,20 +43,53 @@ class SynchronizationModule(contacts.AbstractContactSynchronizationModule):
         """
         contacts.AbstractContactSynchronizationModule.__init__(self,  verbose,  soft,  modulesString,  config,  configsection,  "Contacts Evolution")
         self.verbose = verbose
+        self._path = config.get(configsection,'path')
+        self._rawData = {}
+        self._edsIDs = {}
         pisiprogress.getCallback().verbose("Evolution contacts module loaded")
-            
-        # pull configuration data and whatever initialization might be required
+
 
     def load(self):
         """
         Load all data from backend
         """
-        pass    # implementation goes here
+        pisiprogress.getCallback().verbose ("Evolution: Loading")
+        pisiprogress.getCallback().progress.push(0, 100)
+        file = bsddb.hashopen(self._path)
+        pisiprogress.getCallback().update("Loading")
+        amount = len(file.keys())
+        i = 0
+        for key in file.keys():
+            data = file[key]
+            if not data.startswith('BEGIN:VCARD'):
+                continue
+            comps = vobject.readComponents(data[:len(data)-1])  # there is some problem with a traling white space in each entry
+            for x in comps:
+                print x.n.value.given
 
+                
+                atts = extractVcfEntry(x)
+
+                id = contacts.assembleID(atts)
+                c = contacts.Contact(id,  atts)
+                self._allContacts[id] = c
+                self._rawData[id] = x
+                self._edsIDs[id] = key
+                i+=1
+                pisiprogress.getCallback().progress.setProgress((i*100) / amount)
+                pisiprogress.getCallback().update('Loading')                
+        pisiprogress.getCallback().progress.drop()
+    
     def saveModifications( self ):
         """
         Save whatever changes have come by
         """
+        pisiprogress.getCallback().verbose("Evolution module: I apply %d changes now" %(len(self._history)))
+
+        if len(self._history) == 0:
+            return      # don't touch anything if there haven't been any changes        
+        
+        file = bsddb.hashopen(self._path)
         for listItem in self._history:
             action = listItem[0]
             id = listItem[1]
@@ -64,8 +98,10 @@ class SynchronizationModule(contacts.AbstractContactSynchronizationModule):
                 pass    # implementation goes here
             elif action == ACTIONID_DELETE:
                 pisiprogress.getCallback().verbose("\t\t<evolution contacts> deleting %s" %(id))
-                pass    # implementation goes here
+                key = self._edsIDs[id]
+                del file[key]
             elif action == ACTIONID_MODIFY:
                 pisiprogress.getCallback().verbose("\t\t<evolution contacts> replacing %s" %(id))
                 pass    # implementation goes here
  
+        file.sync()
--- tests/evolutiontest.py
+++ tests/evolutiontest.py
@@ -1,10 +1,40 @@
-from evolution import ebook, ecal
 
-print ebook.list_addressbooks()
 
-personal = ebook.open_addressbook('default')
+def tryLibWay():
+    from evolution import ebook, ecal
 
-for c in personal.get_all_contacts():
-    print ("*** %s" %(c.get_name()))
-    print ("\t%s, %s" %(c.props.family_name, c.props.given_name))
-    print ("\t%s" %(c.props.email_1))
+    print ebook.list_addressbooks()
+
+    personal = ebook.open_addressbook('default')
+
+    for c in personal.get_all_contacts():
+        print ("*** %s" %(c.get_name()))
+        print ("\t%s, %s" %(c.props.family_name, c.props.given_name))
+        print ("\t%s" %(c.props.email_1))
+
+
+# this program helped
+
+def tryDodgyWay():
+    import bsddb
+    import vobject  
+    file = bsddb.hashopen("/home/michael/.evolution/addressbook/local/system/addressbook.db")
+    rem = None
+    print len(file.keys())
+    for key in file.keys():
+        data = file[key]
+        if not data.startswith('BEGIN:VCARD'):
+            continue
+        
+        comps = vobject.readComponents(data[:len(data)-1])
+        for x in comps:
+            print x.n.value.given
+#            if x.n.value.given == "Michael":
+#                rem = [key, data, x]
+#                x.n.value.given = "KichKasch"
+#
+#        
+#        file[rem[0]] = rem[2].serialize()
+#        file.sync()
+        
+tryDodgyWay()
